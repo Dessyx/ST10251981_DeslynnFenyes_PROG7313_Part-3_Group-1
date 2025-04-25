62754b1af63a7ff4b370a78022ede87eed97fb79
Deslynn <deslynnfenyes000@gmail.com>
2025-04-25 09:06:00 +0200

    updated commit

--- app/src/main/java/com/example/prog7313_groupwork/entities/Budget.kt
+++ app/src/main/java/com/example/prog7313_groupwork/entities/Budget.kt
@@ -3,7 +3,7 @@ package com.example.prog7313_groupwork.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "budgets")
+@Entity(tableName = "budget")
 data class Budget(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,

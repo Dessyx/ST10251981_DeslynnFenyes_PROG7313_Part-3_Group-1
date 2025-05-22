b1d0059b472d7f3ac4b74a15f1e51bec4cf6be5b
debbydelport <debby@rampoint.info>
2025-05-22 14:54:36 +0200

    test commit

--- app/src/main/java/com/example/prog7313_groupwork/DashboardActivity.kt
+++ app/src/main/java/com/example/prog7313_groupwork/DashboardActivity.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+//testing
 // ----------------------- Functionality for dashboard.xml ------------------------------
 class DashboardActivity : AppCompatActivity() {
     private lateinit var monthSpinner: Spinner
